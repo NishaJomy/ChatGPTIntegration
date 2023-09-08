0e7744e7a7a1b53943b34528c31f8a82967fc20b
Nisha George <nishajomy.2007@gmail.com>
2023-09-07 19:39:38 -0500

    modified controller and postmapping

--- ChatGPTIntegration/src/main/java/org/launchcode/ChatGPTIntegration/controller/ChatGPTController.java
+++ ChatGPTIntegration/src/main/java/org/launchcode/ChatGPTIntegration/controller/ChatGPTController.java
@@ -4,23 +4,23 @@ import com.theokanning.openai.completion.CompletionRequest;
 import com.theokanning.openai.completion.CompletionResult;
 import com.theokanning.openai.completion.chat.ChatCompletionRequest;
 import com.theokanning.openai.service.OpenAiService;
+import org.launchcode.ChatGPTIntegration.dto.ChatMessagePrompt;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class ChatGPTController {
+
     @GetMapping("/getChat/{prompt}")
-    public CompletionResult getPrompt(@PathVariable String prompt){
-        ///v1/completions (Legacy)	text-davinci-003, text-davinci-002, text-davinci-001, text-curie-001, text-babbage-001, text-ada-001, davinci, curie, babbage, ada
-        OpenAiService service = new OpenAiService("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");
+    public String getPrompt(@PathVariable String prompt) {
 
-        CompletionRequest completionRequest = CompletionRequest.builder()
-                .prompt(prompt)
-                .model("text-davinci-003")
-                .echo(true)
-                .build();
-       return service.createCompletion(completionRequest);
-
+        // /v1/completion -> text-davinci-003, text-davinci-002, text-curie-001,
+        // text-babbage-001, text-ada-001
+        OpenAiService service = new OpenAiService("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");
+        CompletionRequest completionRequest = CompletionRequest.builder().prompt(prompt).model("text-davinci-003")
+                .echo(true).build();
+        return service.createCompletion(completionRequest).getChoices().get(0).getText();
     }
+
     @PostMapping("/chat")
     public String getChatMessages(@RequestBody ChatMessagePrompt prompt) {
 
@@ -33,6 +33,5 @@ public class ChatGPTController {
                 .model("gpt-3.5-turbo-16k").build();
         return service.createChatCompletion(completionRequest).getChoices().get(0).getMessage().getContent();
     }
-
 
 }
